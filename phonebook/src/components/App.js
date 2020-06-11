e5f3212b4c53b0ced13784ed69fe143f121abb97
Maulana Rahman <maulana.auliar@gmail.com>
2020-06-11 13:07:12 +0700

    add name form handler, add form input handler, display name

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react'
 
+const PhonebookNames = ({persons}) => {
+    return (
+        <div>
+            {persons.map(person=>
+                <p key={person.name}>{person.name}</p>    
+            )}
+        </div>
+    )
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleAddName = (event) => {
+    event.preventDefault()
+    //   console.log('add name ' + newName)
+    const new_person = { name: newName }
+    setPersons(persons.concat(new_person))
+    setNewName('')
+  }
+
+  const handleFormNameChange = (event) => {
+      setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleFormNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <PhonebookNames persons={persons}/>
     </div>
   )
 }
